958877ae209985d28f46471823d36e795cedcc93
AbruptSoftware <dylannichols702@gmail.com>
2022-11-05 13:08:42 -0400

    Created Form for Savings Goal
    This is in index right now but it'll be in its own separate page later :)

--- hackncapp.py
+++ hackncapp.py
@@ -1,15 +1,23 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, render_template
- 
+from flask import Flask, render_template, request
+from datetime import datetime
+
+# Define Savings Goal class
+class SavingsGoal:
+    def __init__(self, name, goal, deadline):
+        self.name = name
+        self.goal = goal
+        self.deadline = deadline
+
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
- 
+
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/')
+@app.route('/', methods =["GET", "POST"])
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return render_template('index.html')
