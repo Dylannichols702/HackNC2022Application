bc26714c377435389c3165835975f09f9dcd6c5d
Michael Maggiore <michaelpmaggiore@gmail.com>
2022-11-05 12:41:05 -0400

    Changed foreign keys

--- hacknc.sql
+++ hacknc.sql
@@ -15,20 +15,20 @@ CREATE TABLE category(
 DROP TABLE IF EXISTS login CASCADE;
 CREATE TABLE login(
     user_id TEXT PRIMARY KEY,
-    password TEXT
-    username TEXT,
+    password UNIQUE TEXT
+    username UNIQUE TEXT,
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 DROP TABLE IF EXISTS payment CASCADE;
 CREATE TABLE payment (
     payment_id SERIAL PRIMARY KEY,
-    user_id SERIAL REFERENCES person(id),
+    user_id SERIAL,
     name TEXT,
     cost double precision,  -- might change to save space 
     category_name TEXT REFERENCES category(name),
     type_of_payment TEXT,
     due_date DATE
-
+    FOREIGN KEY (user_id) REFERENCES user(id)
 
 );
